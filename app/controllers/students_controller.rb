70820bb9abd578076bae953574dd5c475280fb0d
rrosztoczy <rrosztoczy@gmail.com>
2019-03-15 11:07:22 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,31 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  # before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    set_student
+    redirect_to action: "show", id: @student.id
+    # render :activate
   end
 
   private
 
     def set_student
+      puts "in the method!"
       @student = Student.find(params[:id])
+      if @student.active == true
+        puts "in the in!"
+        Student.update(@student.id, :active => false)
+      else
+        Student.update(@student.id, :active => true)
+      end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+test_student1 = Student.create(first_name: "test1", last_name: "student1")
+test_student2 = Student.create(first_name: "test2", last_name: "student2")
+test_student3 = Student.create(first_name: "test3", last_name: "student3")
+test_student4 = Student.create(first_name: "test4", last_name: "student4")
+test_student5 = Student.create(first_name: "test5", last_name: "student5")
